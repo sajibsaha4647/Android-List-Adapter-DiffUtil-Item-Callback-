a461173a1a7838b32e8ba1ef3b40e1bfc90ee5ba
sajibsaha4647 <sksaha4648@gmail.com>
2022-06-02 00:54:47 +0600

    updated

---
+++ app/src/main/java/com/example/listadapter/programmingAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.listadapter
+
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.View
+import android.widget.ListAdapter
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.listadapter.programmingAdapter.programmingViewHolder
+
+class programmingAdapter() : ListAdapter<programmingItem,programmingAdapter.programmingViewHolder> {
+
+//    classss programmingViewHolder(view:View) : RecyclerView.ViewHolder(view){
+//
+//        val initial = view.findViewById<TextView>(R.id.initial)
+//        val name = view.findViewById<TextView>(R.id.name)
+//
+//        fun bind(item: programmingItem){
+//
+//          initial.text = item.initial
+//            name.text = item.name
+//
+//        }
+//
+//    }
+
+}
---
+++ app/src/main/java/com/example/listadapter/programmingItem.kt
@@ -0,0 +1,4 @@
+package com.example.listadapter
+
+data class programmingItem(val id:Int, val initial:String,val name:String)
+
